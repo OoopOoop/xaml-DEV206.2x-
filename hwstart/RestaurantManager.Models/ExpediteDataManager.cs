5b2b50ba294836bcb45775bc86c9e2abfd2d7f9b
OoopOoop <anastasiiabaranova@yahoo.co.uk>
2015-11-26 17:10:46 +0000

    Module1. HomeWork#1
    Added INotifyPropertyChanged implementation to DataManager class and
    updated OnDataLoaded methods.

---
+++ hwstart/RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            OnPropertyChanged(nameof(OrderItems));
+        }
+
+        public List<Order> OrderItems
+        {
+            get { return base.Repository.Orders; }
+
+        }
+    }
+}
